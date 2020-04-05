bfe03eb00a0ceea98ebcde9d8cdd579f763e89a3
Sofya Povarova <sofya.povarova@mail.ru>
2020-04-05 12:48:58 +0300

    Updates

---
+++ precache-manifest.bcb4fa0081150e521748a5b2680ddc2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a7c87d15d816f54f6ca17ca63070d2",
+    "url": "/react-sequence-alignment/index.html"
+  },
+  {
+    "revision": "b6811a7f847de9da057b",
+    "url": "/react-sequence-alignment/static/css/main.4a5f322d.chunk.css"
+  },
+  {
+    "revision": "1f02a3c7ba3b3dbec55d",
+    "url": "/react-sequence-alignment/static/js/2.6d748c9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sequence-alignment/static/js/2.6d748c9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6811a7f847de9da057b",
+    "url": "/react-sequence-alignment/static/js/main.f73fdba3.chunk.js"
+  },
+  {
+    "revision": "2a9a3c7ae3622663d353",
+    "url": "/react-sequence-alignment/static/js/runtime-main.c0b024d5.js"
+  },
+  {
+    "revision": "4616d4117af402411a549788e588bb61",
+    "url": "/react-sequence-alignment/static/media/dna.4616d411.png"
+  }
+]);
